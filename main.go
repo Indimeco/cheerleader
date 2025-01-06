e7022cc48bc476f82ff456e8d756c9b98dbcbe59
Indimeco <18373729+Indimeco@users.noreply.github.com>
2025-01-06 13:36:32 +1100

    init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello",
+	}, nil
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
