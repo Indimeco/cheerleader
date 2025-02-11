681153a1e762302c53d8e48d322b2d80ed4f6369
Indimeco <18373729+Indimeco@users.noreply.github.com>
2025-02-11 14:40:09 +1100

    refactor: move response ownership to handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,9 +2,13 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log/slog"
+	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/indimeco/cheerleader/internal/api"
 	"github.com/indimeco/cheerleader/internal/ddb"
 	"github.com/indimeco/cheerleader/internal/models"
 )
@@ -32,55 +36,127 @@ func New(ctx context.Context) (Handler, error) {
 	}, nil
 }
 
-func (h Handler) PutScore(ctx context.Context, score models.Score) error {
-	err := h.Database.PutScore(ctx, score)
+func (h Handler) PutScore(ctx context.Context, apiDefinition api.ApiDefinition, body string) events.APIGatewayProxyResponse {
+	score, err := models.NewScore(apiDefinition.Game, apiDefinition.PlayerId, body)
 	if err != nil {
-		return fmt.Errorf("Failed to put score: %w", err)
+		return h.ResponseBadRequest(err)
+	}
+	err = h.Database.PutScore(ctx, score)
+	if err != nil {
+		return h.ResponseInternalServerError(fmt.Errorf("Failed to put score: %w", err))
 	}
 
-	return nil
+	return h.ResponseCreated()
 }
 
-func (h Handler) GetTopPlayerScores(ctx context.Context, scoreRequest models.PlayerScoreRequest) ([]models.Score, error) {
+func (h Handler) GetTopPlayerScores(ctx context.Context, apiDefinition api.ApiDefinition, params map[string]string) events.APIGatewayProxyResponse {
+	scoreRequest, err := models.NewPlayerScoreRequest(params, apiDefinition.Game, apiDefinition.PlayerId)
+	if err != nil {
+		return h.ResponseBadRequest(err)
+	}
 	scores, err := h.Database.GetTopPlayerScores(ctx, scoreRequest)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to get top player scores: %w", err)
+		return h.ResponseInternalServerError(fmt.Errorf("Failed to get top player scores: %w", err))
 	}
-	return scores, nil
+	out, err := json.Marshal(&scores)
+	if err != nil {
+		return h.ResponseInternalServerError(err)
+	}
+	return h.ResponseOk(string(out))
 }
 
-func (h Handler) GetTopRanks(ctx context.Context, ranksRequest models.RanksRequest) (models.Ranks, error) {
+func (h Handler) GetTopRanks(ctx context.Context, apiDefinition api.ApiDefinition, params map[string]string) events.APIGatewayProxyResponse {
+	ranksRequest, err := models.NewRanksRequest(params, apiDefinition.Game)
+	if err != nil {
+		return h.ResponseBadRequest(err)
+	}
 	ranks, err := h.Database.GetTopRanks(ctx, ranksRequest)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to get top ranks: %w", err)
+		return h.ResponseInternalServerError(fmt.Errorf("Failed to get top ranks: %w", err))
 	}
-	return ranks, nil
+	out, err := json.Marshal(&ranks)
+	if err != nil {
+		return h.ResponseInternalServerError(err)
+	}
+	return h.ResponseOk(string(out))
 }
 
-func (h Handler) GetRanksAroundPlayer(ctx context.Context, game string, playerId string, around int) (models.Ranks, error) {
+func (h Handler) GetRanksAroundPlayer(ctx context.Context, apiDefinition api.ApiDefinition, params map[string]string) events.APIGatewayProxyResponse {
+	ranksRequest, err := models.NewPlayerRanksRequest(params, apiDefinition.Game, apiDefinition.PlayerId)
+	if err != nil {
+		return h.ResponseBadRequest(err)
+	}
+
 	playerScores, err := h.Database.GetTopPlayerScores(ctx, models.PlayerScoreRequest{
-		PlayerId:     playerId,
-		ScoreRequest: models.ScoreRequest{Game: game, Limit: 1},
+		PlayerId:     apiDefinition.PlayerId,
+		ScoreRequest: models.ScoreRequest{Game: apiDefinition.Game, Limit: 1},
 	})
 	if err != nil {
-		return nil, fmt.Errorf("Failed to get top player score: %w", err)
+		return h.ResponseInternalServerError(fmt.Errorf("Failed to get top player score: %w", err))
 	}
+
 	// Player hasn't scored yet
 	if len(playerScores) < 1 {
-		return models.Ranks{}, nil
+		return h.ResponseOk("[]")
 	}
 	topScore := playerScores[0]
 	ranks, err := h.Database.GetTopRanks(ctx, models.RanksRequest{})
 	if err != nil {
-		return nil, fmt.Errorf("Failed to get top ranks: %w", err)
+		return h.ResponseInternalServerError(fmt.Errorf("Failed to get top ranks: %w", err))
 	}
 
 	index := ranks.BinarySearch(topScore.Score, 0, len(ranks))
 	// Player is not ranked
 	if index == -1 {
-		return models.Ranks{}, nil
+		return h.ResponseOk("[]")
 	}
-	ranksAround := ranks.Around(index, around)
+	ranksAround := ranks.Around(index, ranksRequest.Around)
 
-	return ranksAround, nil
+	out, err := json.Marshal(&ranksAround)
+	if err != nil {
+		return h.ResponseInternalServerError(err)
+	}
+	return h.ResponseOk(string(out))
+}
+
+func (h Handler) ResponseInternalServerError(err error) events.APIGatewayProxyResponse {
+	h.Logger.Error(fmt.Sprintf("Unexpected error: %v", err))
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusInternalServerError,
+		Body:       "Internal server error",
+	}
+}
+
+func (h Handler) ResponseOk(data string) events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       data,
+	}
+}
+
+func (h Handler) ResponseCreated() events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusCreated,
+	}
+}
+
+func (h Handler) ResponseBadRequest(err error) events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{
+		Body:       fmt.Sprint(err),
+		StatusCode: http.StatusBadRequest,
+	}
+}
+
+func (h Handler) ResponseMethodNotAllowed() events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusMethodNotAllowed,
+		Body:       "Method not allowed",
+	}
+}
+
+func (h Handler) ResponseNotFound() events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusNotFound,
+		Body:       "Resource not found",
+	}
 }
--- main.go
+++ main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/indimeco/cheerleader/internal/api"
 	"github.com/indimeco/cheerleader/internal/handler"
-	"github.com/indimeco/cheerleader/internal/models"
 )
 
 func handleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -24,11 +21,11 @@ func handleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (ev
 	apiRoutes := api.NewApiRoutes()
 	apiDefinition, err := api.EventPathToApiDefinition(event.Path)
 	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusNotFound,
-			Body:       "Resource not found",
-		}, nil
+		return h.ResponseNotFound(), nil
 	}
+
+	params := event.QueryStringParameters
+	body := event.Body
 
 	switch apiDefinition.Route {
 	case apiRoutes.ScoresByPlayer:
@@ -36,119 +33,30 @@ func handleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (ev
 
 			switch event.HTTPMethod {
 			case "GET":
-				params := event.QueryStringParameters
-				scoreRequest, err := models.NewPlayerScoreRequest(params, apiDefinition.Game, apiDefinition.PlayerId)
-				if err != nil {
-					return events.APIGatewayProxyResponse{
-						StatusCode: http.StatusBadRequest,
-						Body:       fmt.Sprint(err),
-					}, nil
-				}
-				scores, err := h.GetTopPlayerScores(ctx, scoreRequest)
-				if err != nil {
-					return internalServerError(&h, err), err
-				}
-				out, err := json.Marshal(&scores)
-				if err != nil {
-					return internalServerError(&h, err), err
-				}
-				return events.APIGatewayProxyResponse{
-					Body:       string(out),
-					StatusCode: http.StatusOK,
-				}, nil
-
+				return h.GetTopPlayerScores(ctx, apiDefinition, params), nil
 			case "PUT":
-				score, err := models.NewScore(apiDefinition.Game, apiDefinition.PlayerId, event.Body)
-				if err != nil {
-					return events.APIGatewayProxyResponse{
-						Body:       fmt.Sprint(err),
-						StatusCode: http.StatusBadRequest,
-					}, nil
-				}
-				err = h.PutScore(ctx, score)
-				if err != nil {
-					return internalServerError(&h, err), err
-				}
-				return events.APIGatewayProxyResponse{
-					StatusCode: http.StatusCreated,
-				}, nil
+				return h.PutScore(ctx, apiDefinition, body), nil
 			default:
-				return events.APIGatewayProxyResponse{
-					StatusCode: http.StatusMethodNotAllowed,
-					Body:       "Method not allowed",
-				}, nil
+				return h.ResponseMethodNotAllowed(), nil
 			}
 		}
 	case apiRoutes.RanksByPlayer:
 		{
 			if event.HTTPMethod != "GET" {
-				return events.APIGatewayProxyResponse{
-					StatusCode: http.StatusMethodNotAllowed,
-					Body:       "Method not allowed",
-				}, nil
+				return h.ResponseMethodNotAllowed(), nil
 			}
-			ranksRequest, err := models.NewPlayerRanksRequest(event.QueryStringParameters, apiDefinition.Game, apiDefinition.PlayerId)
-			if err != nil {
-				return events.APIGatewayProxyResponse{
-					Body:       fmt.Sprint(err),
-					StatusCode: http.StatusBadRequest,
-				}, nil
-			}
-			ranksAround, err := h.GetRanksAroundPlayer(ctx, ranksRequest.Game, ranksRequest.PlayerId, ranksRequest.Around)
-			if err != nil {
-				return internalServerError(&h, err), err
-			}
-			out, err := json.Marshal(&ranksAround)
-			if err != nil {
-				return internalServerError(&h, err), err
-			}
-			return events.APIGatewayProxyResponse{
-				Body:       string(out),
-				StatusCode: http.StatusOK,
-			}, nil
+			return h.GetRanksAroundPlayer(ctx, apiDefinition, params), nil
 		}
 	case apiRoutes.Ranks:
 		{
 			if event.HTTPMethod != "GET" {
-				return events.APIGatewayProxyResponse{
-					StatusCode: http.StatusMethodNotAllowed,
-					Body:       "Method not allowed",
-				}, nil
+				return h.ResponseMethodNotAllowed(), nil
 			}
-			ranksRequest, err := models.NewRanksRequest(event.QueryStringParameters, apiDefinition.Game)
-			if err != nil {
-				return events.APIGatewayProxyResponse{
-					StatusCode: http.StatusBadRequest,
-					Body:       fmt.Sprint(err),
-				}, nil
-			}
-			ranks, err := h.GetTopRanks(ctx, ranksRequest)
-			if err != nil {
-				return internalServerError(&h, err), err
-			}
-			out, err := json.Marshal(&ranks)
-			if err != nil {
-				return internalServerError(&h, err), err
-			}
-			return events.APIGatewayProxyResponse{
-				Body:       string(out),
-				StatusCode: http.StatusOK,
-			}, nil
+			return h.GetTopRanks(ctx, apiDefinition, params), nil
 		}
 	}
 
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusInternalServerError,
-		Body:       "Failed to handle request",
-	}, nil
-}
-
-func internalServerError(handler *handler.Handler, err error) events.APIGatewayProxyResponse {
-	handler.Logger.Error(fmt.Sprintf("Unexpected error: %v", err))
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusInternalServerError,
-		Body:       "Internal server error",
-	}
+	return h.ResponseInternalServerError(fmt.Errorf("Unhandled API escaped with path %q method %q ", event.Path, event.HTTPMethod)), nil
 }
 
 func main() {
